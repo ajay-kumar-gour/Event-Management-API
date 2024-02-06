5848d9d9c530868fc3c3b1a134eefba77445f14a
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-06 12:01:19 +0530

    Built an EXPRESS Server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("Hello EVENT MANAGEMENT API");
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+console.log(PORT);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    sucess: "True",
+    message: "Welcome to Event Management API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});

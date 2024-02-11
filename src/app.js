5a9f4a4cfc7179b9759e210dfd0760a1a385c9d2
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-11 17:45:57 +0530

    Intialized swagger-ui-epress & swagger-JSDoc

--- src/app.js
+++ src/app.js
@@ -3,12 +3,43 @@ require("dotenv").config();
 const colors = require("colors");
 const connectToDB = require("./Database/DB-Connect");
 const app = express();
+
+const swaggerUi = require("swagger-ui-express");
+// const swaggerDocument = require("./swagger.json");
+const swaggerJsdoc = require("swagger-jsdoc");
+
 const PORT = process.env.PORT;
 console.log(PORT);
 app.use(express.json());
 const eventRouter = require("./Routes/eventRoutes");
 
 app.use("/events", eventRouter);
+
+const options = {
+  definition: {
+    failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+    openapi: "3.0.0",
+    info: {
+      title: "Event Management API",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000", // Replace this with your server URL
+        description: "Local development server",
+      },
+      {
+        url: "https://eventmanagemntapi.com",
+        description: "Production server",
+      },
+    ],
+  },
+  apis: ["./src/Routes/*.js", "./src/Controllers/*.js"], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
 app.get("/", (req, res) => {
   res.status(200).json({
     sucess: "True",

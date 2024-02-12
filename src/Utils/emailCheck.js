cfb9890af4a7f92ef13519ed003f3984af60a274
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-12 21:44:50 +0530

    feat: Implemented a function to VALIDATE EMAil

---
+++ src/Utils/emailCheck.js
@@ -0,0 +1,8 @@
+const isValidEmail = (email) => {
+  // Regular expression to validate email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+
+module.exports = isValidEmail;

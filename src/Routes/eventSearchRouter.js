976aaced9ca4778bea4ebd89e7f496297f2ba842
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-13 21:28:29 +0530

    feat: Added search endpoints for events
    Created separate endpoints for searching events by event name, location (city), event category, and date range. Each endpoint accepts corresponding query parameters to filter events accordingly.

---
+++ src/Routes/eventSearchRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  searchEventByNameController,
+  searchEventByCityController,
+  searchEventByCategoryController,
+  searchEventByDateRangeController,
+} = require("../Controllers/searchEventController");
+
+const router = express.Router();
+
+router.get("name/:name", searchEventByNameController);
+router.get("city/:city", searchEventByCityController);
+router.get("category/:category", searchEventByCategoryController);
+router.get("dateRange/:dateRange", searchEventByDateRangeController);
+
+module.exports = router;

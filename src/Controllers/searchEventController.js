6c757dd16ef7f74ef85e24e946f75df869ed3663
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-13 21:29:43 +0530

    feat: Add controllers for searching events by name, city, category, and date range

---
+++ src/Controllers/searchEventController.js
@@ -0,0 +1,14 @@
+const Event = require("../Models/eventModels");
+
+const searchEventByNameController = (req, res) => {};
+
+const searchEventByCityController = (req, res) => {};
+const searchEventByCategoryController = (req, res) => {};
+const searchEventByDateRangeController = (req, res) => {};
+
+module.exports = {
+  searchEventByNameController,
+  searchEventByCityController,
+  searchEventByCategoryController,
+  searchEventByDateRangeController,
+};

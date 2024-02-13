69576007ca0ff79d15269b3cfbbf1ac2a1cb1aef
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-13 12:37:17 +0530

    feat:Imlemented a ROUTE to handle LOGIN CONTROLLER

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { registerController } = require("../Controllers/userController");
+const {
+  registerController,
+  loginController,
+} = require("../Controllers/userController");
 const router = express.Router();
 
-
-
-
 router.post("/register", registerController);
-
+router.post("/login", loginController);
 
 module.exports = router;

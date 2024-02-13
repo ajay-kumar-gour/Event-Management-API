ff26f8853e344df73e6670a028b72ce64e5f2c88
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-13 12:11:46 +0530

    feat: Impemented a PRE Middleware to HASH password before saving it in DB using BCRYPT

--- src/Models/userModels.js
+++ src/Models/userModels.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -31,6 +31,25 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+// Hash the password before saving
+// if we use arrow fun istead of regular fun, error is given that isModified is not a func beacuse, arrow fun
+//do not have thie this context, they inherit it from their surrounding coide when they are defined
+// so we need to use a regular normal function as callback here
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 

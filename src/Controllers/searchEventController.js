610132bf3835a809222de6d48854d988f799d865
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-14 13:10:54 +0530

    feat: Implemented CONTROLLER to handle SEARCH EVENT BY NAME

--- src/Controllers/searchEventController.js
+++ src/Controllers/searchEventController.js
@@ -1,10 +1,33 @@
 const Event = require("../Models/eventModels");
 
-const searchEventByNameController = async(req, res) => {
-
+const searchEventByNameController = async (req, res) => {
   const eventName = req.params.name;
 
+  
+  try {
+    const eventNameFilteredData = await Event.find({
+      name: { $regex: new RegExp(eventName, "i") },
+    });
 
+    if (!eventNameFilteredData) {
+      return res.status(404).send({
+        success: false,
+        message: "No Event Found with provided event",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Event Found",
+      totalEventFound: eventNameFilteredData.length,
+      event: eventNameFilteredData,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 };
 
 const searchEventByCityController = (req, res) => {};

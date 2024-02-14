1057fe318442b9b11c02b7f8d4d2232da5e6d412
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-14 13:08:45 +0530

    Modified: Corrected ROUTES defination

--- src/Routes/eventSearchRouter.js
+++ src/Routes/eventSearchRouter.js
@@ -8,9 +8,9 @@ const {
 
 const router = express.Router();
 
-router.get("name/:name", searchEventByNameController);
-router.get("city/:city", searchEventByCityController);
-router.get("category/:category", searchEventByCategoryController);
-router.get("dateRange/:dateRange", searchEventByDateRangeController);
+router.get("/name/:name", searchEventByNameController);
+router.get("/city/:city", searchEventByCityController);
+router.get("/category/:category", searchEventByCategoryController);
+router.get("/dateRange/:dateRange", searchEventByDateRangeController);
 
 module.exports = router;

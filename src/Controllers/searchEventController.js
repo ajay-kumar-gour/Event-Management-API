d70189205a900a75453739c3ce97511193b706f8
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-15 18:28:22 +0530

    feat: Implemented CONTROLLER to handle SEARCH EVENT BY location(CITY)

--- src/Controllers/searchEventController.js
+++ src/Controllers/searchEventController.js
@@ -28,7 +28,36 @@ const searchEventByNameController = async (req, res) => {
     });
   }
 };
+const searchEventByCityController = async (req, res) => {
+  const cityName = req.params.city;
+  console.log("cityName", cityName);
+  try {
+    const eventDataFilteredCity = await Event.find({
+      "location.city": { $regex: new RegExp(cityName, "i") },
+    });
 
+    console.log("eventDataFilteredCity", eventDataFilteredCity);
+    if (eventDataFilteredCity.length == 0) {
+      return res.status(404).send({
+        success: false,
+        message: "No Event found in the provided location(city)",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Event found",
+      totalEvent: eventDataFilteredCity.length,
+      event: eventDataFilteredCity,
+    });
+  } catch (errror) {
+    res.status(500).send({
+      success: false,
+      message: "Internal Server Errior",
+      error: error.message,
+    });
+  }
+};
 
 const searchEventByCategoryController = (req, res) => {};
 const searchEventByDateRangeController = (req, res) => {};

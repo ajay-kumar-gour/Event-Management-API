cb7ed65d6293efbf0345262f372478d45dc467be
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-18 18:29:39 +0530

    feat: Implemented a CONTROLLER to REGISTER USER to a EVENT

---
+++ src/Controllers/eventRegisterController.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const Event = require("../Models/eventModels");
+
+const eventRegisterController = async (req, res) => {
+  const eventID = req.params.eventID;
+  const userData = req.decodedData;
+  const userEmail = userData.email;
+  const userFirstName = userData.firstName;
+  const userLastName = userData.lastName;
+  const userName = userFirstName + " " + userLastName;
+
+  // console.log("username", userName);
+  // console.log(userData);
+  // console.log(eventID);
+  // res.status(200).send({ userEmail, eventID });
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(eventID)) {
+      return res.status(400).send({
+        success: false,
+        message: "Provided Event ID is Invalid",
+      });
+    }
+
+    const eventToRegister = await Event.findById(eventID);
+    if (!eventToRegister) {
+      return res.status(404).status({
+        success: false,
+        message: "No Event exist with provided event ID",
+      });
+    }
+
+    const eventRegistered = await eventToRegister.save();
+
+    res.status(200).send({
+      success: true,
+      message: "user successfully registered to the event",
+      eventRegistered,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = eventRegisterController;

364a6b00202115c347b4e2a021e0894c30a63e3f
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-18 15:07:27 +0530

    Defined : ROUTE Handler for SEARCH EVENT BY DATEs

--- src/Routes/eventSearchRouter.js
+++ src/Routes/eventSearchRouter.js
@@ -12,8 +12,7 @@ const router = express.Router();
 router.get("/name/:name", searchEventByNameController);
 router.get("/city/:city", searchEventByCityController);
 router.get("/category/:category", searchEventByCategoryController);
-router.get("/dateRange/:dateRange", searchEventByDateRangeController);
-
+router.get("/dateRange", searchEventByDateRangeController);
 router.get("/price/:priceType", searchEventByPriceTypeController);
 
 module.exports = router;
